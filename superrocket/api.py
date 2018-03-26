4b25541bca0b7f6895959c20f47e2eb8a0fbce04
Ankur Srivastava <ankur.srivastava@recogizer.de>
2018-03-26 23:19:05 +0200

    updated message

--- superrocket/api.py
+++ superrocket/api.py
@@ -17,7 +17,6 @@ class Api(object):
             webhook (str) : A complete RocketChat webhook url
             hostname (str): A hostname to represent where the message is being sent from
             botname (str) : Readable botname for this bot, defaults to superrocket
-            verify (bool) : requests verify parameter to verify server certificates
             emoji (str)   : Default emoji to be published with this message
         Returns:
 
@@ -27,14 +26,14 @@ class Api(object):
         self.hostname = kwargs.get('hostname', socket.gethostname())
         self.botname = kwargs.get('botname', "superrocket")
         self.attachment = kwargs.get('attachment', None)
-        self.verify = kwargs.get('verify', True)
         self.emoji = kwargs.get('emoji', ':rocket:')
 
-    def send(self, msg):
+    def send(self, msg, verify=True):
         """Send a given message to rocketchat webhook.
 
         Args:
             msg (str) : Message to be published on the webhook. This could be a markdown as well.
+            verify (bool) : requests verify parameter to verify server certificates
         """
         payload = {
             'channel': self.channel,
@@ -43,7 +42,7 @@ class Api(object):
             'emoji': self.emoji,
             'attachments': [{"text": self.attachment, "color": "#ff0000"}],
         }
-        self.__post_message(url=self.webhook, data=payload, verify=self.verify)
+        self.__post_message(url=self.webhook, data=payload, verify=verify)
 
     def __post_message(self, url, data, verify):
         """Send a post request to a given webhook url."""
